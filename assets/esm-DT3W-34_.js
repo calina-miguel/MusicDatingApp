59b67ecd62cd5a51df96f69a779861f57230eb5a
Miguel Calina <calina.miguel@gmail.com>
2026-09-22 21:31:04 +0800

    Update app bundle

--- assets/esm-DT3W-34_.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-k-kBmVRw.js","assets/index-DhirUq9-.js","assets/index-Dk4l0pSU.css"])))=>i.map(i=>d[i]);
-import{n as e,r as t}from"./index-DhirUq9-.js";var n=e(`App`,{web:()=>t(()=>import(`./web-k-kBmVRw.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
---
+++ assets/esm-u8aiwJsY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CW1zphmo.js","assets/index-Dd50mNm-.js","assets/index-Dk4l0pSU.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-Dd50mNm-.js";var n=e(`App`,{web:()=>t(()=>import(`./web-CW1zphmo.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
