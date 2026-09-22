bb4a1815695462f9e7553cd495fca9b05e6e285a
Miguel Calina <calina.miguel@gmail.com>
2026-09-22 17:43:53 +0800

    Update SoundSync app

---
+++ assets/esm-BqWdxMtj.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C4allM_T.js","assets/index-B86BjNX-.js","assets/index-Dk4l0pSU.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-B86BjNX-.js";var n=e(`Browser`,{web:()=>t(()=>import(`./web-C4allM_T.js`).then(e=>new e.BrowserWeb),__vite__mapDeps([0,1,2]))});export{n as Browser};
